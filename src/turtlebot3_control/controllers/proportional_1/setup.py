9d937b88def9fa76f347b0b43615083d71ea01d3
JackCahill24 <johncahill4493@gmail.com>
2025-02-10 16:01:36 -0500

    remove logs

--- src/turtlebot3_control/controllers/proportional_1/setup.py
+++ src/turtlebot3_control/controllers/proportional_1/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'turtlebot3_navcontrol'
+package_name = 'proportional_1'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'turtlebot3_navcontrol = turtlebot3_navcontrol.turtlebot3_navcontrol:main',
+            'proportional_1 = proportional_1.proportional_1:main',
         ],
     },
 )
